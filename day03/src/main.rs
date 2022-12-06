c5143e8c8343cb7951b16863ffad06fa9a596bee
David Marx <david.marx84@gmail.com>
2022-12-05 19:10:54 -0500

    housekeeping

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,25 +1,21 @@
 use std::convert::TryInto;
 
+
 fn rucksack2compartments(rucksack: &str, n_compartments: Option<u32>) -> Vec<String> {
     let n: usize= n_compartments.unwrap_or(2).try_into().unwrap(); // set default compartments = 2
-    //let len = rucksack.chars().count();
-    //let len: u32 = rucksack.chars().count().try_into().unwrap();
-    let len: usize = rucksack.len().try_into().unwrap();
-    let compartment_len = len / n;
-    let mut string = String::new();
-    let mut outv = Vec::<String>::new();
+    let compartment_len = rucksack.len() / n;
+    let mut compartment = String::new();
+    let mut outv = Vec::<String>::new(); // vector of compartments
     for (i,c) in rucksack.chars().enumerate() {
-        //println!("{} {}", i, c);
-
-        string.push(c);
+        compartment.push(c);
         if (i+1) % compartment_len == 0 {
-            outv.push(string.clone());
-            string="".to_string();
+            outv.push(compartment.clone());
+            compartment="".to_string();
         }
     }
-    //("foo".to_string(),"bar".to_string())
     outv
 }
+
 
 fn main() {
     println!("Hello, world!");
