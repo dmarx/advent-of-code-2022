feff904f9c5ac2c8fdf1dac851b8c76f1d01d064
David Marx <david.marx84@gmail.com>
2022-12-05 19:07:11 -0500

    rucksack2compartment for day3p1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,31 @@
+use std::convert::TryInto;
+
+fn rucksack2compartments(rucksack: &str, n_compartments: Option<u32>) -> Vec<String> {
+    let n: usize= n_compartments.unwrap_or(2).try_into().unwrap(); // set default compartments = 2
+    //let len = rucksack.chars().count();
+    //let len: u32 = rucksack.chars().count().try_into().unwrap();
+    let len: usize = rucksack.len().try_into().unwrap();
+    let compartment_len = len / n;
+    let mut string = String::new();
+    let mut outv = Vec::<String>::new();
+    for (i,c) in rucksack.chars().enumerate() {
+        //println!("{} {}", i, c);
+
+        string.push(c);
+        if (i+1) % compartment_len == 0 {
+            outv.push(string.clone());
+            string="".to_string();
+        }
+    }
+    //("foo".to_string(),"bar".to_string())
+    outv
+}
+
 fn main() {
     println!("Hello, world!");
+    let outv = rucksack2compartments("foobar", Some(2));
+    println!("{}", outv.len());
+    for compartment in outv {
+        println!("{}", compartment);
+    }
 }
